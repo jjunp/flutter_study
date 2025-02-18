356a3052c4e6ff9f335a15ebfe4a5f3df0dce330
jjunp <jjun@comerstone.com>
2025-02-18 15:33:52 +0900

    Big_wheel

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,165 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BigWheelApp());
+}
+
+class BigWheelApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Big Wheel Game',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: BigWheelScreen(),
+    );
+  }
+}
+
+class BigWheelScreen extends StatefulWidget {
+  @override
+  _BigWheelScreenState createState() => _BigWheelScreenState();
+}
+
+class _BigWheelScreenState extends State<BigWheelScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _wheelController;
+  late Animation<double> _wheelAnimation;
+  late AnimationController _arrowController;
+  late Animation<double> _arrowAnimation;
+  bool _isSpinning = false;
+  double _currentAngle = 0;
+
+  final Map<int, String> numberToGrade = {
+    for (int i = 1; i <= 24; i++) i: 'SILVER',
+    for (int i = 25; i <= 39; i++) i: 'GOLD',
+    for (int i = 40; i <= 46; i++) i: 'EMERALD',
+    for (int i = 47; i <= 50; i++) i: 'DIAMOND',
+    51: 'CRISTAL',
+    52: 'CRISTAL',
+    53: 'JOKER',
+    54: 'MEGA',
+  };
+
+  int _currentNumber = 1;
+  String _currentGrade = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _wheelController = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+
+    _wheelAnimation = Tween<double>(begin: 0, end: 0).animate(
+      CurvedAnimation(parent: _wheelController, curve: Curves.decelerate),
+    );
+
+    _arrowController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _arrowAnimation = Tween<double>(begin: -0.1, end: 0.1).animate(
+      CurvedAnimation(parent: _arrowController, curve: Curves.easeInOut),
+    );
+  }
+
+  void _startSpin() {
+    if (_isSpinning) return;
+
+    _arrowController.repeat(reverse: true);
+    final int randomTurns = Random().nextInt(3) + 3;
+    final double endAngle =
+        _currentAngle + (360 * randomTurns) + Random().nextDouble() * 360;
+
+    _wheelAnimation = Tween<double>(
+      begin: _currentAngle,
+      end: endAngle,
+    ).animate(
+      CurvedAnimation(parent: _wheelController, curve: Curves.decelerate),
+    );
+
+    _wheelController.forward(from: 0);
+    setState(() => _isSpinning = true);
+
+    _wheelController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _arrowController.stop();
+        setState(() {
+          _isSpinning = false;
+          _currentAngle = endAngle % 360;
+          int totalSections = numberToGrade.length;
+          int index =
+              ((_currentAngle ~/ (360 / totalSections)) + 1) % totalSections;
+          _currentNumber = index == 0 ? totalSections : index;
+          _currentGrade = numberToGrade[_currentNumber] ?? 'UNKNOWN';
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Big Wheel Game')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: _arrowAnimation,
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: _arrowAnimation.value,
+                child: Icon(Icons.arrow_drop_down, size: 70, color: Colors.red),
+              );
+            },
+          ),
+          Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                AnimatedBuilder(
+                  animation: _wheelAnimation,
+                  builder: (context, child) {
+                    return Transform.rotate(
+                      angle: _wheelAnimation.value * pi / 180,
+                      child: Image.asset('assets/big_wheel.png', width: 400),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _startSpin,
+            child: Text(_isSpinning ? 'Spinning...' : 'Spin'),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Number: $_currentNumber',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Grade: $_currentGrade',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _wheelController.dispose();
+    _arrowController.dispose();
+    super.dispose();
+  }
+}
